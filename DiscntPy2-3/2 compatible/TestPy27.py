ee6d98b7579c0ce007fb8fd6a46a48d693c874b7
Zian Jolo M. Catacutan <86049869+xenz25@users.noreply.github.com>
2021-08-01 18:37:53 +0800

    Added Test Files for Module 2.7

---
+++ DiscntPy2-3/2 compatible/TestPy27.py
@@ -0,0 +1,15 @@
+import DiscntPy27 
+##
+x = SD()
+
+x.discnt_inputs([0,1970,0,6,90],[' A',' P','D',' d',' tm'],4)
+x.save('-','Computation 1','a')
+x.show_inputs('')
+x.show_computed('')
+
+x.formula_for('discount rate',12)
+x.formula_for('dISCO UNT',' ')
+
+
+ 
+
