afae1ca6df062146d8306671288acd48c61a8fba
Zian Jolo M. Catacutan <86049869+xenz25@users.noreply.github.com>
2021-08-01 18:36:52 +0800

    Added Test Python Modules

---
+++ DiscntPy2-3/3 compatible/TestPy.py
@@ -0,0 +1,43 @@
+from DiscntPy import *
+a = 1298
+x = Simple_Discount()
+##print (x.accepted_var)
+
+x.formula_for('amount','’This is the new Title')
+
+
+x.discnt_inputs([2000,0,30,0,90],['A','P','D','d','td_o_actual'],4)
+x.show_inputs('c')
+x.show_computed('z')
+##x.save('-','Computation 1','a')
+
+##c = SD()
+##c.discnt_inputs([1234,0,22.715,0,90],['A','P','D','d','td_o_actual'],4)
+##c.show_inputs('c')
+##c.show_computed('z')
+##c.save('-','Computation 2','w')
+
+##x.Discount 
+##x.Proceed 
+##x.dis_rate
+##x.ty 
+
+##x.save('','computations 2','w')
+
+##
+##y = SD()
+##y.show_inputs('@')
+##y.show_computed('@')
+##y.save('@','computations 3')
+##y.discnt_inputs([2345,0,0,12,90],['A','P','D','d','td_o_actual'],4)
+##
+##
+##                                                                     
+##
+###print (x.Amount, x.Discount, x.Proceed, x.dis_rate, x.ty)
+##
+##
+##
+
+##
+##help(SD)
